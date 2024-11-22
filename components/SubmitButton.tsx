51bd12dedb76344d444e743b38b69d61867286a4
ahmed-saleh98 <ahmed.mohamed.saleh1998@gmail.com>
2024-11-22 23:44:55 +0200

    update home page ui

---
+++ components/SubmitButton.tsx
@@ -0,0 +1,34 @@
+import { Button } from './ui/button';
+import Image from 'next/image';
+
+interface ButtonProps {
+  children: React.ReactNode;
+  className?: string;
+  isLoading: boolean;
+}
+const SubmitButton = ({ isLoading, className, children }: ButtonProps) => {
+  return (
+    <Button
+      type="submit"
+      disabled={isLoading}
+      className={className ?? 'shad-primary-btn w-full'}
+    >
+      {isLoading ? (
+        <div className="flex items-center gap-4">
+          <Image
+            src="/assets/icons/loader.svg"
+            alt="Loader"
+            width={24}
+            height={24}
+            className="animate-spin"
+          />
+          Loading ...
+        </div>
+      ) : (
+        children
+      )}
+    </Button>
+  );
+};
+
+export default SubmitButton;
